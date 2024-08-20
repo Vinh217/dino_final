79dba6425a3c290334c8df719f417c04539b40d1
Vinh_DQ <53872492+Vinh217@users.noreply.github.com>
2024-08-20 09:52:43 +0700

    add social

--- src/layouts/footer.jsx
+++ src/layouts/footer.jsx
@@ -7,11 +7,16 @@ export default function Footer() {
             className="flex aspect-video w-full max-w-full flex-col items-center justify-center bg-footer bg-100% bg-center bg-no-repeat pr-[16px] md:h-[300px]"
             id="footer"
         >
-            <div className="flex gap-[50px]" 
-            data-aos="fade-in"
-            >
-                <PiTelegramLogoDuotone className="h-[70px] w-[70px] cursor-pointer text-white" />
-                <BsTwitterX className="h-[70px] w-[70px] cursor-pointer text-white" />
+            <div className="flex gap-[50px]" data-aos="fade-in">
+                <a href="https://t.me/dino_meme" target="_blank">
+                    <PiTelegramLogoDuotone className="h-[70px] w-[70px] cursor-pointer text-white" />
+                </a>
+                <a href="https://t.me/Dino_memecoin" target="_blank">
+                    <PiTelegramLogoDuotone className="h-[70px] w-[70px] cursor-pointer text-white" />
+                </a>
+                <a href="https://x.com/dino_memecoin" target="_blank">
+                    <BsTwitterX className="h-[70px] w-[70px] cursor-pointer text-white" />
+                </a>
             </div>
             <Paragraph classes={'font-[700] mt-[20px] text-center'}>
                 Unleash the Dino-Mite! Stay Connected and Join the Adventure!
