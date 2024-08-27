a284ab954e7262cb0fb2760cc292129ec25f49b9
vinhdq <doquangvinhnt@gmail.com>
2024-08-27 07:30:53 +0700

    update presale

--- src/components/PreSale.jsx
+++ src/components/PreSale.jsx
@@ -6,7 +6,7 @@ import Paragraph from './common/Paragraph';
 import ButtonCustom from './common/ButtonCustom';
 
 export default function PreSale() {
-    const dateTimeAfter30_8 = new Date('2024-08-30T00:00:00').getTime();
+    const dateTimeAfter30_8 = new Date('2024-08-31T22:00:00').getTime();
 
     return (
         <Container>
